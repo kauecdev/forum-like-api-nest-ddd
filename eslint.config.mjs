f1a428e5fd2a8f52ca4b1e5985365c667459ac11
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-09-25 20:47:33 -0300

    inital commit: project created and eslint setup

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import path from 'node:path'
+import url from 'node:url'
+import { FlatCompat } from '@eslint/eslintrc'
+
+export default [
+  ...new FlatCompat({
+    baseDirectory: path.dirname(url.fileURLToPath(import.meta.url)),
+  }).config({
+    ignores: ['node_modules', 'dist'],
+    env: {
+      es2021: true,
+      node: true,
+    },
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          singleQuote: true,
+          trailingComma: 'all',
+          arrowParens: 'always',
+          semi: false,
+          endOfLine: 'auto',
+        },
+      ],
+    },
+    settings: {
+      'import/parsers': {
+        [import('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+      },
+    },
+  }),
+]
