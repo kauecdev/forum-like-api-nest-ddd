e777d5458a9f67271ad8929e340a1200f99820c2
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-10-06 10:48:57 -0300

    feat: added new tables, created mappers

---
+++ src/infra/database/prisma/mappers/prisma-answer-mapper.ts
@@ -0,0 +1,29 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
+import { Prisma, Answer as PrismaAnswer } from '@prisma/client'
+
+export class PrismaAnswerMapper {
+  static toDomain(raw: PrismaAnswer): Answer {
+    return Answer.create(
+      {
+        authorId: new UniqueEntityId(raw.authorId),
+        questionId: new UniqueEntityId(raw.questionId),
+        content: raw.content,
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityId(raw.id),
+    )
+  }
+
+  static toPersistence(answer: Answer): Prisma.AnswerUncheckedCreateInput {
+    return {
+      id: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      questionId: answer.questionId.toString(),
+      content: answer.content,
+      createdAt: answer.createdAt,
+      updatedAt: answer.udpatedAt,
+    }
+  }
+}
