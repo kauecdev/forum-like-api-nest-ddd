78f2deec22ec4065e7dd7602aabab0f81520edb8
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-10-12 22:20:35 -0300

    feat: creatd controller to answer question and e2e test

---
+++ src/infra/http/controllers/answer-question.controller.ts
@@ -0,0 +1,41 @@
+import { BadRequestException, Param } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
+
+const answerQuestionBodySchema = z.object({
+  content: z.string(),
+})
+
+type AnswerQuestionBodySchema = z.infer<typeof answerQuestionBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(answerQuestionBodySchema)
+
+@Controller('/questions/:questionId/answers')
+export class AnswerQuestionController {
+  constructor(private answerQuestion: AnswerQuestionUseCase) {}
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: AnswerQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+    @Param('questionId') questionId: string,
+  ) {
+    const { content } = body
+    const userId = user.sub
+
+    const result = await this.answerQuestion.execute({
+      content,
+      authorId: userId,
+      attachmentsIds: [],
+      questionId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
